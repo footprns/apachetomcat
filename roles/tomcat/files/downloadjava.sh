d2d9e19e5eb8c1047c09bc3997fd2085329250b4
Iman Kurniawan <imankurniawan@gmail.com>
2017-11-30 23:00:57 +0530

    added java download using script module

---
+++ roles/tomcat/files/downloadjava.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd $1
+#wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jre-8u151-linux-x64.tar.gz
